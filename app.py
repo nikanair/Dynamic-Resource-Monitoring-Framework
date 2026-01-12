1f8f8d930740e84ad94721918ea6c173ee45ccf3
Sowparnika Nair <sowparnikanair17@gmail.com>
2026-01-11 20:36:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ import matplotlib.pyplot as plt
 import subprocess
 import re
 
-# Creating nearly an infinite for loop to monitor the details continuously
 for i in range(10):
-    # CPU and RAM
     cpu_usage = psutil.cpu_percent()
     mem_usage = psutil.virtual_memory().percent
 
@@ -28,12 +26,10 @@ for i in range(10):
     print(mem_usage)
     print(gpu_usage)
 
-    # Creating the scatter plot
     plt.scatter(i, cpu_usage, color = "red")
     plt.scatter(i, mem_usage, color = "blue")
     plt.scatter(i, gpu_usage, color = "green")
     plt.legend(["CPU", "Memory", "GPU"], loc ="lower right")
     plt.pause(0.05)
 
-    # Plotting the information
 plt.show()
